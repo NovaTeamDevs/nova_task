3841bb7398bf530eb83a8ba45e7957bfcdc61496
mohammad <m.deheghanifard@gmail.com>
2024-05-05 08:11:26 +0330

    refactor TodoChartScreen ui(Changing the background color of the chart display cards)

--- lib/views/todo_chart_screen.dart
+++ lib/views/todo_chart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:nova_task/core/resources/app_colors.dart';
 import 'package:nova_task/core/resources/app_strings.dart';
 import 'package:nova_task/core/resources/dimens.dart';
 import 'package:nova_task/core/widgets/app_chart_widget.dart';
@@ -24,13 +25,8 @@ class TodoChartScreen extends StatelessWidget {
               padding: const EdgeInsets.all(Dimens.medium * 2),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(16.r),
-                  color: const Color(0xFFF9FAFD),
-                  boxShadow: [
-                    BoxShadow(
-                        color: context.theme.colorScheme.shadow,
-                        blurRadius: 15,
-                        offset: const Offset(-3, 7))
-                  ]),
+                  color: AppColors.lightRedColor.withOpacity(0.10),
+                  ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -47,13 +43,8 @@ class TodoChartScreen extends StatelessWidget {
               padding: const EdgeInsets.all(Dimens.medium * 2),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(16.r),
-                  color: const Color(0xFFF9FAFD),
-                  boxShadow: [
-                    BoxShadow(
-                        color: context.theme.colorScheme.shadow,
-                        blurRadius: 15,
-                        offset: const Offset(-3, 7))
-                  ]),
+                  color: AppColors.blueColor.withOpacity(0.15),
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
