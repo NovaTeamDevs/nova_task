ae4c4f3d5d654848854bdce72adb0760da926095
mohammad <m.deheghanifard@gmail.com>
2024-05-05 08:28:06 +0330

    impl category model class

---
+++ lib/models/category_model.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class CategoryModel {
+
+  final String? iconPath;
+  final String? title;
+  final Color? bgColor;
+
+  CategoryModel({this.iconPath, this.title, this.bgColor});
+}
